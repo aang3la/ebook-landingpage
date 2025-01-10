33e5a566f3d4a521eb050009a5ee60eb654095a3
Angela Antova <antovaangela@gmail.com>
2025-01-10 15:46:32 +0100

    commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import '../app.css';
+
+    let {children} = $props();
+</script>
+
+{@render children()}
+
+<!-- we can use it besides this, we can render the children like
+<slot /> -->
